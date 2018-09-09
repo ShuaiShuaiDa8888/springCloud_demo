1ef650288b7bd888bc3e69371564415f7464b5c0
weiShuai <601770050@qq.com>
2018-09-09 15:32:31 +0800

    新增服务提供者：provide_server

---
+++ provide_server/src/main/java/com/weishuai/controller/MemberController.java
@@ -0,0 +1,24 @@
+package com.weishuai.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by WS on 2018/9/9.
+ */
+@RestController
+public class MemberController {
+
+    @GetMapping(value = "getMemberAll")
+    public List<String> getMemberAll(){
+        List<String> memberList = new ArrayList<>();
+        memberList.add("zhangsan");
+        memberList.add("Lisi");
+        memberList.add("wangwu");
+
+        return memberList;
+    }
+}
